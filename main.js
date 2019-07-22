538cc4492ec91abbe4bde87a1148285d66be10fa
Camo-Master <ben.mcm@vodafone.co.nz>
2019-07-22 15:08:39 +1200

    Starting thing

---
+++ main.js
@@ -0,0 +1,20 @@
+const ashs = './ashs.json';
+const no = ashs;
+
+const day1 = [
+  "p1": "spanish"
+]
+
+function load(){
+
+  alert(no);
+}
+
+
+function click(){
+  alert("yees");
+}
+
+function myFunction() {
+  alert(day1[0])
+}
