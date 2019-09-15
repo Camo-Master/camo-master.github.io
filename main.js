958196815b0feb303362eb8aac8bba33710662bf
Camo-Master <ben.mcm@vodafone.co.nz>
2019-09-15 17:47:04 +1200

    Big Changes

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+var playable = true;
+var x = document.getElementById("rolled");
+
 setTimeout(openWindows, 3000);
 
 function openWindows(){
+  if(playable){
+    try{
+      x.play();
+    } catch(e){
+      console.log(e);
+    }
+  }
+}
 
-
-
+function yeet(){
+  x.pause();
+  playable = false;
 }
