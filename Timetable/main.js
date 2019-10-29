6604f86f72eb52e7d76abb24a65eb027009e61e5
Camo-Master <ben.mcm@vodafone.co.nz>
2019-10-29 22:57:35 +1300

    Fixed Bug

--- Timetable/main.js
+++ Timetable/main.js
@@ -13,12 +13,12 @@ $(document).ready(function(){
     defaultValues();
 
     $("#personChoice").change(function(){
-      loadDays(0)
-      loadTimes($('#personChoice').val(), $('#dayChoice').val())
+      loadDays(0);
+      loadTimes($('#personChoice').val(), $('#dayChoice').val()-1);
     });
 
     $("#dayChoice").change(function(){
-      loadTimes($('#personChoice').val(), $('#dayChoice').val())
+      loadTimes($('#personChoice').val(), $('#dayChoice').val()-1);
     });
 
 
@@ -28,13 +28,13 @@ $(document).ready(function(){
 function defaultValues(){
   let personList = Object.keys(people);
 
-  console.log(personList.length);
+
   for(i=0; i<personList.length; i++){
     let option = $(`<option value="${personList[i]}"></option>`).text(personList[i]);
     if(personList[i] == "Ben"){
       option.attr("selected", "");
     }
-    console.log(option);
+
 
     $('#personChoice').append(option);
 
@@ -47,13 +47,12 @@ function defaultValues(){
 function loadDays(selected){
   $("#dayChoice").html('');
   let thisSchool = schools[people[ $("#personChoice").val()].school];
-  console.log(thisSchool);
+
   for(j=1; j<thisSchool.days+1; j++){
     let option = $(`<option value="${j}"></option>`).text("Day: " + j);
     if(j == ""){
       option.attr("selected", "");
     }
-    console.log(option);
     $('#dayChoice').append(option);
 
   }
