394496e27e099b619abf13c460ebcf0b00b7963e
Camo-Master <ben.mcm@vodafone.co.nz>
2019-10-29 22:53:15 +1300

    Change person from in program

--- Timetable/main.js
+++ Timetable/main.js
@@ -5,18 +5,62 @@ let timetableWrapper;
 
 
 $(document).ready(function(){
-  timetableWrapper = document.getElementById('timetableWrapper');
+  timetableWrapper = $('#timetableWrapper');
   $.getJSON('info.json', function(data){
     people = data.people;
     schools = data.schools;
 
-    loadTimes("Ben", 0);
+    defaultValues();
+
+    $("#personChoice").change(function(){
+      loadDays(0)
+      loadTimes($('#personChoice').val(), $('#dayChoice').val())
+    });
+
+    $("#dayChoice").change(function(){
+      loadTimes($('#personChoice').val(), $('#dayChoice').val())
+    });
+
 
   });
-
 });
 
+function defaultValues(){
+  let personList = Object.keys(people);
+
+  console.log(personList.length);
+  for(i=0; i<personList.length; i++){
+    let option = $(`<option value="${personList[i]}"></option>`).text(personList[i]);
+    if(personList[i] == "Ben"){
+      option.attr("selected", "");
+    }
+    console.log(option);
+
+    $('#personChoice').append(option);
+
+  }
+  loadTimes("Ben", 0);
+  loadDays(0);
+}
+
+
+function loadDays(selected){
+  $("#dayChoice").html('');
+  let thisSchool = schools[people[ $("#personChoice").val()].school];
+  console.log(thisSchool);
+  for(j=1; j<thisSchool.days+1; j++){
+    let option = $(`<option value="${j}"></option>`).text("Day: " + j);
+    if(j == ""){
+      option.attr("selected", "");
+    }
+    console.log(option);
+    $('#dayChoice').append(option);
+
+  }
+}
+
 function loadTimes(person, day){
+  $("#timetableWrapper").html('');
   let school = schools[people[person].school];
   person = people[person];
   let timeArray = school.timeGaps;
@@ -29,11 +73,9 @@ function loadTimes(person, day){
     lessonBox.setAttribute('style', `height:${timeArray[i]*3-5}px;`);
 
     textBox = document.createElement("div");
-    let textBoxString;
+    let textBoxString = `<b>${school.lessonNames[i]}:</b>`;
 
-    if(lesson === "N"){
-      textBoxString = `<b>${school.lessonNames[i]}:</b>`;
-    }else{
+    if(lesson !== "N"){
       textBoxString = `<b>${school.lessonNames[i]}:</b> ${person.subjects[lesson-1]}`;
     }
 
@@ -42,9 +84,7 @@ function loadTimes(person, day){
 
     lessonBox.appendChild(textBox);
 
-    console.log(lessonBox)
-
-    timetableWrapper.appendChild(lessonBox);
+    timetableWrapper.append(lessonBox);
 
 
 
