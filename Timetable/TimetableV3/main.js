0d843e488bcde282eb423d5ca43cffd0e838cc6f
Camo-Master <ben.mcm@vodafone.co.nz>
2020-01-21 20:57:09 +1300

    Tab system

--- Timetable/TimetableV3/main.js
+++ Timetable/TimetableV3/main.js
@@ -14,7 +14,6 @@ let timtableFormat = [
 
 $(document).ready(function(){
 
-
   let table = $("#timetable");
   let tr = $("<tr> </tr>");
   let i = 1;
@@ -57,4 +56,23 @@ $(document).ready(function(){
   table.append(tr);
 
 
+  openTab("welcomeTab");
 });
+
+
+
+
+function openTab(tab) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  console.log(tab);
+  document.getElementById(tab).style.display = "block";
+  document.getElementById(tab+"Link").className += " active";
+}
