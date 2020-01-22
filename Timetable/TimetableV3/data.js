a4f972d4f05479e6906afbae2db661288b427681
Camo-Master <ben.mcm@vodafone.co.nz>
2020-01-22 14:24:15 +1300

    Restructure/Tidy

---
+++ Timetable/TimetableV3/data.js
@@ -0,0 +1,181 @@
+let timtableFormat = [
+  [1, 2, 3, 4, 5],
+  [6, 1, 2, 3, 4],
+  [5, 7, 6, 1, 2],
+  [3, 4, 5, 6, 1],
+  [2, 3, 4, 5, 6],
+  [5, 4, 3, 2, 1],
+  [4, 3, 2, 1, 6],
+  [2, 7, 1, 6, 5],
+  [1, 6, 5, 4, 3],
+  [6, 5, 4, 3, 2]
+
+]
+
+let peopleList = [
+  {"name": "Ben", "id": 0},
+  {"name": "Nick", "id": 1},
+  {"name": "Bread", "id": 2},
+  {"name": "Marizanne", "id": 3},
+  {"name": "Gretel", "id": 4},
+  {"name": "Nathan", "id": 5},
+  {"name": "Michaela", "id": 6},
+  {"name": "Alex", "id": 7},
+  {"name": "Lexie", "id": 8},
+  {"name": "Bridie", "id": 9}
+
+];
+
+
+let classes = {
+  "line1Classes": {
+    "Physics" : {
+      "Teacher" : "Mr Yates",
+      "Class": "K8",
+      "People": [0, 1, 4],
+      "Code": "L2PHY"
+    },
+    "Chemistry" : {
+      "Teacher" : "Mrs Caldwell ",
+      "Class": "B3",
+      "People": [3, 7, 8],
+      "Code": "L2BIO"
+    },
+    "Business" : {
+      "Teacher" : "Mrs West ",
+      "Class": "A3",
+      "People": [5],
+      "Code": "L2BUS"
+    },
+    "Maths" : {
+      "Teacher" : "Mrs Light ",
+      "Class": "B15",
+      "People": [6, 9],
+      "Code": "L2MA2"
+    }
+  },
+ "line2Classes" : {
+    "Chemistry" : {
+      "Teacher" : "Mrs Caldwell",
+      "Class": "K3",
+      "People": [0, 1, 5, 9],
+      "Code": "L2CHE"
+    },
+    "Drama" : {
+      "Teacher" : "Mr Dutton",
+      "Class": "DRA",
+      "People": [3, 6, 8],
+      "Code": "L2DRA"
+    },
+    "Biology" : {
+      "Teacher" : "Mr Walker",
+      "Class": "K5",
+      "People": [4, 7],
+      "Code": "L2BIO"
+    }
+  },
+ "line3Classes" : {
+    "Biology" : {
+      "Teacher" : "Mrs Moran",
+      "Class": "K1",
+      "People": [0, 3, 5, 9],
+      "Code": "L2BIO"
+    },
+    "Design" : {
+      "Teacher" : "Mr Pulsford",
+      "Class": "D4/5",
+      "People": [1, 6],
+      "Code": "L2DES"
+    },
+    "Graphics" : {
+      "Teacher" : "STE",
+      "Class": "T2",
+      "People": [4],
+      "Code": "L2DVC"
+    },
+    "Maths" : {
+      "Teacher" : "BUK",
+      "Class": "B3",
+      "People": [7, 8],
+      "Code": "L2MA1"
+    }
+  },
+ "line4Classes" : {
+    "Maths" : {
+      "Teacher" : "Mrs Light",
+      "Class": "B15",
+      "People": [0, 1],
+      "Code": "L2MA1"
+    },
+    "English" : {
+      "Teacher" : "CRO",
+      "Class": "E1",
+      "People": [3, 4, 5, 6, 7],
+      "Code": "L2EN1"
+    },
+    "Geography " : {
+      "Teacher" : "Mrs Gipps",
+      "Class": "A5",
+      "People": [8, 9],
+      "Code": "L2GEO"
+    }
+  },
+ "line5Classes" : {
+    "English" : {
+      "Teacher" : "Mrs Ruland",
+      "Class": "E8",
+      "People": [0, 1, 9],
+      "Code": "L2EN1"
+    },
+    "Media" : {
+      "Teacher" : "Mr King",
+      "Class": "A2",
+      "People": [3, 6, 7, 8],
+      "Code": "L2MED"
+    },
+    "Chemistry" : {
+      "Teacher" : "Mrs McMillan",
+      "Class": "K6",
+      "People": [4],
+      "Code": "L2CHE"
+    },
+    "Maths" : {
+      "Teacher" : "NEV",
+      "Class": "B1",
+      "People": [5],
+      "Code": "L2MA1"
+    }
+  },
+  "line6Classes" : {
+    "Digi Tech" : {
+      "Teacher" : "Mr Schnetler",
+      "Class": "B13",
+      "People": [0, 1, 2],
+      "Code": "L2DIT"
+    },
+    "Maths" : {
+      "Teacher" : "Mr Welch",
+      "Class": "B6",
+      "People": [3, 4],
+      "Code": "L2MA1"
+    },
+    "Physics" : {
+      "Teacher" : "Mrs McMillan",
+      "Class": "K7",
+      "People": [5, 7],
+      "Code": "L2PHY"
+    },
+    "Painting" : {
+      "Teacher" : "Mrs Maday",
+      "Class": "D1",
+      "People": [6, 9],
+      "Code": "L2PAI"
+    },
+    "English" : {
+      "Teacher" : "SHI",
+      "Class": "E2",
+      "People": [8],
+      "Code": "L2EN1"
+    }
+  }
+};
