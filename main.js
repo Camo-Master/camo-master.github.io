2916515fc544b8f12a8650a37ec2bee3f4d517f7
Camo-Master <ben.mcm@vodafone.co.nz>
2020-04-05 00:33:42 +1300

    Improved Splash Page

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 var playable = true;
 var x = document.getElementById("rolled");
+let openPlay = false;
 
 setTimeout(openWindows, 3000);
 
 function openWindows(){
   if(playable){
+    openPlay = true;
     try{
       x.play();
     } catch(e){
       console.log(e);
     }
+  }
+}
+
+function lateLoad() {
+  if(openPlay){
+    x.play();
   }
 }
 
